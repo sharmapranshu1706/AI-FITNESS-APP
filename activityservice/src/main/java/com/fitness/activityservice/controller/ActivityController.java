8599feed965e7fbda6465de5844fec2e8230bf62
Pranshu Sharma <sharma.pranshu1706@gmail.com>
2026-06-21 15:00:20 +0530

    Get User Activity by Activity Set

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -1,8 +1,13 @@
 package com.fitness.activityservice.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.fitness.activityservice.dto.ActivityRequest;
@@ -19,6 +24,14 @@ public class ActivityController {
     @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
         return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId) {
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
+    }
+    @GetMapping("/{activityId}")
+    public ResponseEntity<Object> getActivity(@PathVariable String activityId) {
+        return ResponseEntity.ok(activityService.getActivitiesById(activityId));
     }
 
 }
--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -1,5 +1,8 @@
 package com.fitness.activityservice.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import com.fitness.activityservice.ActivityRepository;
 import com.fitness.activityservice.dto.ActivityRequest;
@@ -38,6 +41,18 @@ public class ActivityService {
         response.setCreatedAt(activity.getCreatedAt());
         response.setUpdatedAt(activity.getUpdatedAt());
         return response;
+    }
+    public List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> activities = activityRepository.findByUserId(userId);
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+
+    }
+    public Object getActivitiesById(String activityId) {
+        return activityRepository.findById(activityId)
+        .map(this::mapToResponse)
+        .orElseThrow(() -> new RuntimeException("Activity not found with id: " + activityId));
     }
         
 }
